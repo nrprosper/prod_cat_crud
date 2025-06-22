c247743fd2650a75f7e268b0a7ef7719f30fd800
NISHIMWE RUKUNDO Prosper <prosper.rk1@gmail.com>
2025-06-22 10:05:55 +0200

    project initialization

---
+++ src/test/java/org/devkiki/crud/ProductCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package org.devkiki.crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductCrudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
